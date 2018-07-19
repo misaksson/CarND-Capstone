d312b5c6e24fed9cba24eb3df2035c3c92de62c7
Gaber <gaber.ayoubi@gmail.com>
2018-07-20 00:16:53 +0200

    add LISA dataset script

---
+++ ext_datasets/get_LISA.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+wget http://cvrr.ucsd.edu/vivachallenge/data/Lights_Detection/LISA_TL_dayTrain.zip
+
+unzip LISA_TL_dayTrain.zip
---
+++ ext_datasets/reformat_LISA.py
@@ -0,0 +1,71 @@
+#!/bin/python
+
+import csv
+import glob
+import os
+import cv2
+
+
+def category(c):
+    return {
+     "go": 0,
+     "stop": 1,
+     "warning": 2
+    }.get(c, 3)
+
+
+def create_boxes(origin, file_n, points):
+    try:
+        img = cv2.imread(file_n)
+        size_x = img.shape[1]
+        size_y = img.shape[0]
+    except:
+        return
+    print(file_n)
+
+    path = 'images/' + os.path.basename(origin)
+    i = 0
+    while(os.path.exists(path + "%s.jpg" % i)):
+            i += 1
+    path = path + str(i)
+
+    for c in points:
+        x = float(c[0]/size_x)
+        y = float(c[1]/size_y)
+        width = float((c[2]-c[0])/size_x)
+        height = float((c[3]-c[1])/size_x)
+        category = c[4]
+        if category == 3:
+            img = cv2.rectangle(img,
+                                (c[0], c[1]),
+                                (c[2], c[3]),
+                                (255, 255, 255), -1)
+        else:
+            f = open(path + '.txt', "a")
+            f.write("%i %f %f %f %f\n" % (category, x, y, width, height))
+            f.close()
+    cv2.imwrite(path + '.jpg', img)
+
+
+def read_from_csv(path, list_):
+    with open(i + '/frameAnnotationsBOX.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        file_n = ''
+        points = []
+        for row in reader:
+            if file_n != (i + "/" + row[0].replace("dayTraining", "frames")):
+                create_boxes(path, file_n, points)
+                file_n = (i + "/" + row[0].replace("dayTraining", "frames"))
+                points = []
+                print(file_n)
+            else:
+                points.append([int(row[2]),
+                               int(row[3]),
+                               int(row[4]),
+                               int(row[5]),
+                               category(row[1])])
+
+os.mkdir("images")
+
+for i in glob.glob('dayTrain/dayClip*'):
+    read_from_csv(i, "")
